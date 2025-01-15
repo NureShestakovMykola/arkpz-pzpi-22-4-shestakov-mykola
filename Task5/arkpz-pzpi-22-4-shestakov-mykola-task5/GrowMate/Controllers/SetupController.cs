7178f2ae7e5cdd2c13527f0c79a1ebf03bb6edd2
SV98R <mykola.shestakov@nure.ua>
2025-01-15 17:58:16 +0200

    Added created code for task 5

---
+++ Task5/arkpz-pzpi-22-4-shestakov-mykola-task5/GrowMate/Controllers/SetupController.cs
@@ -0,0 +1,34 @@
+﻿using Core.Enums;
+using DAL.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GrowMate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SetupController : ControllerBase
+    {
+        private readonly IAdminRepository _adminRepository;
+
+        public SetupController(UnitOfWork unitOfWork)
+        {
+            _adminRepository = unitOfWork.AdminRepository;
+        }
+
+        [HttpPost("setup-database")]
+        public async Task<IActionResult> SetupDatabase()
+        {
+            try
+            {
+                await _adminRepository.SetupDatabaseAsync();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
